5ad395ae9b3c17660cb798d2b39d19a5a4491b0d
khuongnguyeen <khuongnguyen922@gmail.com>
2021-03-29 15:36:36 +0700

    layout horoscope

--- app/src/main/java/fpt/adtrue/horoscope/MainActivity.kt
+++ app/src/main/java/fpt/adtrue/horoscope/MainActivity.kt
@@ -16,25 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        binding.data = App.getViewModel()
-        binding.buttonOk.setOnClickListener {
-            val test = RequestHoroscope()
-            test.sign = "leo"
-            test.day = "today"
-            App.getViewModel().getHoroscope(test.sign,test.day)
-        }
-        binding.buttonYes.setOnClickListener {
-            val test = RequestHoroscope()
-            test.sign = "leo"
-            test.day = "yesterday"
-            App.getViewModel().getHoroscope(test.sign,test.day)
-        }
-        binding.buttonTomo.setOnClickListener {
-            val test = RequestHoroscope()
-            test.sign = "leo"
-            test.day = "tomorrow"
-            App.getViewModel().getHoroscope(test.sign,test.day)
-        }
+
 
     }
 }
--- app/src/main/java/fpt/adtrue/horoscope/api/HoroscopeApi.kt
+++ app/src/main/java/fpt/adtrue/horoscope/api/HoroscopeApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-open interface HoroscopeApi {
+interface HoroscopeApi {
     @POST("/")
     fun getHoroscope(
         @Query("sign") sign:String,
